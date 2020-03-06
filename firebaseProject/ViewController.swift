232445aebad82430cfcd692610f6e64647268a5f
uwagboe <sm.uwagboe@gmail.com>
2020-03-05 20:43:21 -0500

    Initial Commit

---
+++ firebaseProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebaseProject
+//
+//  Created by Pursuit on 3/5/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
